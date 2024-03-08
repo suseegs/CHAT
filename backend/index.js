d231c2672c1c7494eb513195cd0ea3a4be1b9555
suseegs <gssusee2803@gmail.com>
2024-03-08 19:03:18 +0530

    firstcommit

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://deft-blini-e1a304.netlify.app/",
+    origin: "http://localhost:3000",
     credentials: true,
   },
 });
